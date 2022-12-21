1cefcf86b110d1dabd5f5f3c99848c5a5ad56096
Sasanth Thota <66079942+thota-sasanth@users.noreply.github.com>
2022-12-21 03:07:55 -0500

    deleted unnecessary files

--- MyTopo.py
+++
@@ -1,6 +0,0 @@
-from mininet.topo import Topo
-class customTopo(Topo):
-    def build(self):
-        h1 = self.addHost('h1', ip ='192.0.1.1' )
-        h2 = self.addHost('h2', ip ='192.0.1.2 )
-        router = self.addNode('r1', cls=LinuxRouter, ip='193.0.1.1')
